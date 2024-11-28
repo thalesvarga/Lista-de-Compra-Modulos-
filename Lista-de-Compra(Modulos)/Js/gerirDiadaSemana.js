c4934e445ae19afbeff6506f075b1fd2af5f9b44
Thales Varga <thalesfotos@Hotmail.com>
2024-11-28 14:24:53 -0300

    Delete Lista-de-Compra(Modulos) directory

--- Lista-de-Compra(Modulos)/Js/gerirDiadaSemana.js
+++
@@ -1,3 +0,0 @@
-export function gerirDiadaSemana() {
-    return `${new Date().toLocaleDateString("pt-BR", { weekday: "long" })} (${new Date().toLocaleDateString()}) às ${new Date().toLocaleTimeString("pt-BR", { hour: "numeric", minute: "numeric" })}`
-}
